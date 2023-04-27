4fc0bea4be7e23f04561a3202d6f8fcbfccc0c45
Your Name <you@example.com>
2023-04-27 13:33:30 +0700

    add funcs they realiz and routes

--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+
 from .models import ToDo
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 
 def index(request):
-	todos = ToDo.objects.all(ToDo)
+	todos = ToDo.objects.all()
 	return render(request, 'todocore/index.html', {'todo_list': todos, 'title': 'Главная страница'} )
+
+
+@require_http_methods(['POST'])
+def add(request):
+	title = request.POST['title']
+	todo = ToDo(title=title)
+	todo.save()
+	return redirect('index')
+
+
+def update(request, todo_id):
+	todo = ToDo.objects.get(id=todo_id)
+	todo.is_complete = not todo.is_complete
+	todo.save()
+	return redirect('index')
+
+
+def delete(request, todo_id):
+	todo = ToDo.objects.get(id=todo_id)
+	todo.delete()
+	return redirect('index')
